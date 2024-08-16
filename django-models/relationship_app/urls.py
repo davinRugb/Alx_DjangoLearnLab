3599eaf5b91eaf01574e621f03efb76507f7a74e
Davin Rugambwa <davinrugambwa@gmail.com>
2024-08-16 18:41:02 +0200

    relations 4

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -34,3 +34,13 @@ urlpatterns = [
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
 ]
+
+
+from django.urls import path
+from .views import add_book, edit_book, delete_book
+
+urlpatterns = [
+    path('book/add/', add_book, name='add_book'),
+    path('book/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('book/delete/<int:pk>/', delete_book, name='delete_book'),
+]
