5c785ba85df852d446bc7994e026e5ed957fc9cb
Davin Rugambwa <davinrugambwa@gmail.com>
2024-08-24 17:01:21 +0200

    django 10

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,23 +2,23 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 from .models import Article
 
-@permission_required('your_app_name.can_view', raise_exception=True)
-def article_detail(book_list, book):
-    article = get_object_or_404(Article, book=book)
-    return render(book_list, 'article_detail.html', {'article': article})
+@permission_required('bookshelf.book', raise_exception=True)
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, 'article_detail.html', {'article': article})
 
 @permission_required('your_app_name.can_create', raise_exception=True)
-def article_create(book_list):
+def article_create(request):
     # Logic for creating an article
     pass
 
 @permission_required('your_app_name.can_edit', raise_exception=True)
-def article_edit(book_list, book):
+def article_edit(request, pk):
     # Logic for editing an article
     pass
 
 @permission_required('your_app_name.can_delete', raise_exception=True)
-def article_delete(book_list, book):
+def article_delete(request, pk):
     # Logic for deleting an article
     pass
 
