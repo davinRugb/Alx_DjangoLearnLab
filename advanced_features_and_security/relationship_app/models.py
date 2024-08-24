add1f88692689c98b7da1358dc77f615b7a35bfa
Davin Rugambwa <davinrugambwa@gmail.com>
2024-08-24 16:35:51 +0200

    django 7

--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import AbstractUser
+
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
@@ -73,29 +73,7 @@ class Book(models.Model):
         return self.title
 
 
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-    def __str__(self):
-        return self.username
-
-
-class CustomUserManager(CustomUser):
-    def create_user(self, username, email, date_of_birth=None, profile_photo=None, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, date_of_birth=date_of_birth, profile_photo=profile_photo, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(username, email, password=password, **extra_fields)
 
 
 
