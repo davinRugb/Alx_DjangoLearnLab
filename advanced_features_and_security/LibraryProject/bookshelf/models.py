e938caf975af34c10ab7f891e0d31291394428bc
Davin Rugambwa <davinrugambwa@gmail.com>
2024-08-24 16:49:02 +0200

    django 10

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -53,3 +53,19 @@ class CustomUserManager(BaseUserManager):
 
 class SomeModel(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    class Meta:
+        permissions = [
+            ('can_view', 'Can view article'),
+            ('can_create', 'Can create article'),
+            ('can_edit', 'Can edit article'),
+            ('can_delete', 'Can delete article'),
+        ]
+
+    def __str__(self):
+        return self.title
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
-# Create your views here.
+@permission_required('your_app_name.can_view', raise_exception=True)
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, 'article_detail.html', {'article': article})
+
+@permission_required('your_app_name.can_create', raise_exception=True)
+def article_create(request):
+    # Logic for creating an article
+    pass
+
+@permission_required('your_app_name.can_edit', raise_exception=True)
+def article_edit(request, pk):
+    # Logic for editing an article
+    pass
+
+@permission_required('your_app_name.can_delete', raise_exception=True)
+def article_delete(request, pk):
+    # Logic for deleting an article
+    pass
+
+
+
