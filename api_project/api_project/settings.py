f59132fbbdd9c539b4669446774b18083f7a4ca8
Davin Rugambwa <davinrugambwa@gmail.com>
2024-09-04 19:08:47 +0200

    api 4

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',  # Optional: Keep session authentication if needed
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.permissions.IsAuthenticated',
     ),
 }
 
