6216295b811d2a5a4e5feb3f1e35682ea6fe49cc
Davin Rugambwa <davinrugambwa@gmail.com>
2024-09-04 18:14:44 +0200

    api 2

--- api_project/api/serializer.py
+++ api_project/api/serializer.py
@@ -4,4 +4,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ["title", "author", "published_date", "published_at"] 
+        fields = '__all__'
