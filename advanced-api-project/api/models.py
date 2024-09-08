61c96793c0e9d5a2e91827fb80bb8c07f790c830
Davin Rugambwa <davinrugambwa@gmail.com>
2024-09-08 19:09:59 +0200

    advanced api 1

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    published_year = models.DateField()
+    publication_year = models.DateField()
     published_at = models.CharField(max_length=50)
 
 class Author(models.Model):
