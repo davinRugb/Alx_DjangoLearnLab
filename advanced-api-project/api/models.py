5509b53e318b66df45e2be7fb15d62dc91edb559
Davin Rugambwa <davinrugambwa@gmail.com>
2024-09-08 19:08:56 +0200

    advanced api 1

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -5,3 +5,9 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     published_year = models.DateField()
     published_at = models.CharField(max_length=50)
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
